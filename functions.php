1e9781988ddb2781fb0cd18d5cc99a26dc41806f
SaidSultan24 <123152247+SaidSultan24@users.noreply.github.com>
2023-02-15 18:01:16 +0500

    loaded complied css file

--- functions.php
+++ functions.php
@@ -3,5 +3,6 @@
 function fs_styles() {
     wp_enqueue_style( 'sf-main-style', get_template_directory_uri(). '/style.css' );
     wp_enqueue_style( 'sf-fonts', get_template_directory_uri(). '/assets/fonts.css' );
+    wp_enqueue_style( 'sf-styles', get_template_directory_uri(). '/build/index.css' );
   }
   add_action( 'wp_enqueue_scripts', 'fs_styles' );
