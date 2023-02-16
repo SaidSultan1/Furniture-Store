8158f3924257c0d830d49efef41b892c884fa18f
SaidSultan24 <said.onlywp@gmail.com>
2023-02-16 20:57:03 +0500

    Added Owl Carousel Files to the theme

--- functions.php
+++ functions.php
@@ -6,10 +6,22 @@
 
 function lf_styles() {
     wp_enqueue_style( 'lf-main-style', get_template_directory_uri(). '/style.css' );
+    
     wp_enqueue_style( 'lf-fonts', get_template_directory_uri(). '/assets/fonts.css' );
+    
     wp_enqueue_style( 'lf-styles', get_template_directory_uri(). '/build/index.css' );
+    
     wp_enqueue_style("font-awesome","//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
-    wp_enqueue_script( 'lf-scripts', get_template_directory_uri(). '/assets/js/header.js', array('jquery'), true );
+
+    wp_enqueue_script( 'jquery' );
+    
+    wp_enqueue_script( 'lf-main-scripts', get_template_directory_uri(). '/assets/js/ls-main.js', array('jquery'), true );
+    
+    wp_enqueue_script( 'lf-scripts', get_template_directory_uri(). '/build/index.js', array('jquery'), true );
+
+    wp_enqueue_script( 'owl-carousel-js', get_template_directory_uri(). '/inc/owl-carousel/owl.carousel.min.js');
+    
+    wp_enqueue_style( 'owl-carousel-css', get_template_directory_uri(). '/inc/owl-carousel/owl.carousel.min.css');
 }
 add_action( 'wp_enqueue_scripts', 'lf_styles' );
 
