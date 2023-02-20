ecb5d87cc07fd5f68a80239ccb16216a5832c291
SaidSultan24 <said.onlywp@gmail.com>
2023-02-20 16:29:26 +0500

    added WC single product  gallary support

--- functions.php
+++ functions.php
@@ -64,6 +64,10 @@ if( function_exists('acf_add_options_page') ) {
 }
 
 add_action( 'after_setup_theme', 'woocommerce_support' );
+
 function woocommerce_support() {
     add_theme_support( 'woocommerce' );
+    add_theme_support("wc-product-gallery-zoom");
+    add_theme_support("wc-product-gallery-lightbox");
+    add_theme_support("wc-product-gallery-slider");
 }
