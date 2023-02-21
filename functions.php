aeb60099ed8c783aa6c514d3c4fb18db2c591f52
SaidSultan24 <said.onlywp@gmail.com>
2023-02-21 18:30:37 +0500

    Remove default WooCommerce content on shop page

--- functions.php
+++ functions.php
@@ -76,3 +76,17 @@ add_action( 'init', 'remove_woocommerce_content_wrapper_end' );
 function remove_woocommerce_content_wrapper_end() {
     remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
 }
+
+function remove_woocommerce_default_content() {
+  if ( is_shop() ) {
+      remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
+      remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
+      remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
+      remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 );
+      remove_action( 'woocommerce_before_shop_loop', 'woocommerce_output_all_notices', 10 );
+      remove_action( 'woocommerce_after_shop_loop', 'woocommerce_pagination', 10 );
+      remove_action( 'woocommerce_after_shop_loop', 'woocommerce_output_all_notices', 10 );
+  }
+}
+add_action( 'template_redirect', 'remove_woocommerce_default_content' );
+
