408fa9acefc361e708b0f2dbdd70da96ceb4a00c
SaidSultan24 <said.onlywp@gmail.com>
2023-02-24 19:53:10 +0500

    Added single blog template

---
+++ single.php
@@ -0,0 +1,39 @@
+<?php get_header(); ?>
+
+<?php while ( have_posts() ) : the_post(); ?>
+  <div class="single-container">
+    <?php $banner_image = get_field('single_page_background');
+    if (get_field('banner_background')) {
+    ?>
+    <div class="lf-banner-background" style="background-image: url('<?php echo $banner_image['url']; ?>');">
+      <div class="lf-banner-title">
+        <h1><?php the_title(); ?></h1>
+      </div>
+    </div>
+    <?php } ?>
+    <div class="lf-single-content">
+        <?php $content_class = 'width-100';
+        if (get_field('sidebar')) {
+            $content_class = 'width-75';
+        }
+        ?>
+        <div class="lf-single-content-left <?php echo $content_class ?>">
+            <?php if(has_post_thumbnail()): ?>
+            <div class="post-feature-image">
+               <img src="<?php  the_post_thumbnail_url(); ?>"> 
+            </div>
+            <?php
+            endif;
+            the_content(); ?>
+        </div>
+        <?php 
+        if (get_field('sidebar')): ?>
+        <div class="lf-single-content-right">
+            <?php get_sidebar(); ?>
+        </div>
+        <?php endif; ?>
+    </div>
+  </div>
+<?php endwhile; ?>
+
+<?php get_footer(); ?>
